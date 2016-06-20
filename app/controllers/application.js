a26a07a3db7e4a52bb6098a8fec50aa18bc47144
rnbronstein <rnbronstein@gmail.com>
2016-06-20 16:58:05 -0400

    it works

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,15 +6,21 @@ export default Ember.Controller.extend({
   ajax: Ember.inject.service(),
   init: function() {
     //this._super;
+    let result = []
     this.get('ajax').request('visualization').then((response)=>{
-      debugger;
-      // setFormattedTime(response){
+    response.forEach(function(diags){
+      diags.forEach(function(diag){
+        diag.time = (new Date(diag.date));
+        result.push(diag)
+      })
+    })
+      // // setFormattedTime(response){
       //   for (var obj in response){
       //     var t = obj.time;
       //     obj.time = new Date(t);
       //   };
       // };
-      this.set('timeSeriesData', response);
+      this.set('timeSeriesData', result);
     });
   }
 });
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
   timeSeriesData: Ember.computed.reads('applicationController.timeSeriesData'),
-  interval: 'D',
-  fromZero: true
+  interval: 'H',
+  fromZero: true,
+  color: 'rgb(111, 58, 76)',
+  xAxisTitle: true,
+  yAxisTitle: true,
+  xDisplayName: 'Time',
+  yDisplayName: 'Pain Level'
  });
